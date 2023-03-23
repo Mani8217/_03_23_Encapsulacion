1b9431f4b7540f2884abc9448ab51cfeee9b30fc
Mani Shidfar <mreza.shidfar@gmail.com>
2023-03-23 13:15:52 +0100

    1

--- src/com/adecco/modelo/Producto.java
+++ src/com/adecco/modelo/Producto.java
@@ -1,2 +1,50 @@
-package com.adecco.modelo;public class Producto {
+package com.adecco.modelo;
+
+public class Producto {
+
+
+        //1. Attributoss
+        private int numProducto;
+
+        private String nombreProducto;
+
+        private double precio;
+
+        private int cantidadExitencia;
+        //2. metodos
+        //3.constructores
+        //4. Setters y Getters
+       // Asignar valores a Attributos
+
+        public void setNumProducto(int numProducto){
+                this.numProducto = numProducto;
+        }
+
+    public int getNumProducto(){
+          return numProducto;
+    }
+    public void setNombreProducto(String nombreProducto){
+            this.nombreProducto = nombreProducto;
+    }
+    public String getNombreProducto(){
+            return nombreProducto;
+    }
+
+
+    public void setPrecio(double precio){
+            this.precio = precio;
+    }
+    public double getPrecio(){
+            return precio;
+    }
+
+    public int getCantidadExitencia() {
+        return cantidadExitencia;
+    }
+
+    public void setCantidadExitencia(int cantidadExitencia) {
+        this.cantidadExitencia = cantidadExitencia;
+    }
 }
+
+
--- src/com/adecco/presentacion/Main.java
+++ src/com/adecco/presentacion/Main.java
@@ -1,5 +1,17 @@
+package com.adecco.presentacion;
+import com.adecco.modelo.Producto;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Producto p1 = new Producto();
+
+        p1.setNumProducto(1);
+        System.out.println("IdProducto -> " +p1.getNumProducto());
+        p1.setNombreProducto("Raton inalamrico");
+        System.out.println("Nombre de producto -> " + p1.getNombreProducto());
+        p1.setPrecio(22.33);
+        System.out.println("El precio de producto es " + p1.getPrecio());
+        p1.setCantidadExitencia(100);
+        System.out.println("Cantidad en existencia -> " + p1.getCantidadExitencia());
     }
 }
